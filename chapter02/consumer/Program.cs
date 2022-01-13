b438aa64b4903b5521c9646753e912f88b0a112a
Josh Garverick <jgarverick@outlook.com>
2022-01-12 22:38:42 -0500

    Per TR review feedback chapter 2 (simplification of Main and notation regarding CreateHostBuilder)

--- chapter02/consumer/Program.cs
+++ chapter02/consumer/Program.cs
@@ -15,11 +15,17 @@ namespace consumer
             using IHost host = CreateHostBuilder(args).Build();
             var service = new consumerService(_config, topicName);
             await service.Receive();
-            await host.RunAsync();
         }
 
 
-
+        /// <summary>
+        /// Static method used to bootstrap properties, dependency injection, and other environmental settings.
+        /// Normally this would be set up in a more traditional ASP.NET Core web or API app. This method is being used
+        /// mostly for pulling in configuration items that will populate the Kafka consumer configuration.
+        /// Note that AllowAutoCreateTopics is set to true--this will allow you to use any topic name you wish.
+        /// </summary>
+        /// <param name="args">Additional runtime arguments</param>
+        /// <returns>A concrete implementation of the IHostBuilder interface, allowing for the host settings to be instantiated.</returns>
         static IHostBuilder CreateHostBuilder(string[] args) =>
                     Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext, configuration) =>
                     {
@@ -30,6 +36,7 @@ namespace consumer
                                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
                         IConfigurationRoot config = configuration.Build();
                         _config = new ConsumerConfig() { BootstrapServers = config.GetValue<string>("KafkaServer"), GroupId = config.GetValue<string>("DefaultGroupId") };
+                        _config.AllowAutoCreateTopics = true;
                         topicName = config.GetValue<string>("Topic");
                     });
     }
