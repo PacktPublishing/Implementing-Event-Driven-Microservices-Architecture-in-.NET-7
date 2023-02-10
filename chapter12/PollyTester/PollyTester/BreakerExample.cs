d16c9ef8d307c114a59c19bd1b9fe7e6b477b712
Josh Garverick <josh.garverick@gmail.com>
2023-02-10 08:52:48 -0500

    Chapter12 (#12)
    * Updating to .NET 7
    
    * Upgrading to .NET 7, adding retry logic
    
    * Modifications for connectivity worker and failback event
    
    * Stubbing out notification domain and adding operation result classes to Core
    
    * Adding example code

---
+++ chapter12/PollyTester/PollyTester/BreakerExample.cs
@@ -0,0 +1,58 @@
+﻿using Polly;
+
+namespace PollyTester
+{
+    public class ServiceBusinessLogic
+    {
+        public readonly AsyncPolicy retryPolicy;
+        string _returnMessage = "OK";
+
+        public ServiceBusinessLogic()
+        {
+            retryPolicy = Policy.Handle<Exception>().WaitAndRetryAsync(3, retryAttempt =>
+            {
+                return TimeSpan.FromSeconds(retryAttempt++);
+            }, (e, t) =>
+            {
+                Console.WriteLine($"Exception caught for retry: {e.Message}");
+                // Add code to perform additional operations if needed
+            });
+
+
+
+        }
+
+        public async Task<string> TestMethod(string result)
+        {
+            return await retryPolicy.ExecuteAsync<string>(async () =>
+             {
+                 return await Task.Run(async () =>
+                 {
+                     try
+                     {
+                         // Run some nonsense code here to get a failure
+                         var rdm = new Random();
+                         if (rdm.NextInt64() % 2 == 0)
+                         {
+                             throw new InvalidOperationException("Random exception thrown.");
+                         }
+
+                         if (string.IsNullOrEmpty(result.Trim())) throw new ArgumentNullException(paramName: nameof(result), message: "You forgot to add a value.");
+                         _returnMessage = $"String values can be anything you like: {result}";
+
+                         return PolicyResult<string>.Successful(_returnMessage, null).Result;
+                     }
+                     catch (TimeoutException ex)
+                     {
+                         Console.WriteLine("Handled");
+                         return PolicyResult<string>.Failure(ex, ExceptionType.HandledByThisPolicy, null).Result;
+                     }
+                 });
+
+             });
+
+        }
+
+
+    }
+}
---
+++ chapter12/PollyTester/PollyTester/Program.cs
@@ -0,0 +1,56 @@
+using PollyTester;
+using Polly;
+using System.Runtime.CompilerServices;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var breaker = new ServiceBusinessLogic();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(typeof(ServiceBusinessLogic), breaker);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapPost("/testbreaker", async (string stringValue) =>
+{
+    return await breaker.TestMethod(stringValue ?? "Nothing");
+});
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
